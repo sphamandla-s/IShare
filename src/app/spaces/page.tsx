d9a379fd2f5008b80808a725ef970660404cf7ef
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2024-06-25 14:53:22 +0200

    fixed minor bugs

--- src/app/spaces/page.tsx
+++ src/app/spaces/page.tsx
@@ -11,17 +11,19 @@ export default function Spaces() {
             <SpacesHeader />
             <div className="mt-8">
                 <Link href="/search">
-                <button className='mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition'>
-                    Advanced Search
-                </button>
+                    <button className='mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition'>
+                        Advanced Search
+                    </button>
                 </Link>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
                 {spaces.map(space => (
-                    <SpaceCard id={space.id} title={space.title}
-                        description={space.description} image={space.image}
-                        location={space.location} price={space.price}
-                        features={space.features} type={space.type} />
+                    <div key={space.id}>
+                        <SpaceCard id={space.id} title={space.title}
+                            description={space.description} image={space.image}
+                            location={space.location} price={space.price}
+                            features={space.features} type={space.type} />
+                    </div>
                 ))}
             </div>
         </main>
